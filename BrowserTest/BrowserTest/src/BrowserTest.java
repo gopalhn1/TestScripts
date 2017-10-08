f50aa8a6173d69109d872b160fa277f211b17643
gopalhn1 <gopalhn1@gmail.com>
2017-10-08 17:26:12 +0530

    New project BrowserTest created

---
+++ BrowserTest/BrowserTest/src/BrowserTest.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * 
+ */
+
+/**
+ * @author abhij
+ *
+ */
+
+public class BrowserTest {
+
+	/**
+	 * 
+	 */
+	public BrowserTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http:\\www.google.com");
+		Thread.sleep(2000);
+		driver.quit();
+
+	}
+
+}
